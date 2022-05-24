c6cf129b9803d2003bfa7504159f87d3d803148d
karan-push <karan@epns.io>
2022-05-24 13:52:37 +0530

    adding styled components

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 export default {
-    input: 'src/index.tsx',
+    input: 'src/index.ts',
     output: [
       {
         file: pkg.main,
@@ -14,10 +14,10 @@ export default {
         strict: false
       }
     ],
+    globals: { "styled-components": "styled" },
     plugins: [
       sass({ insert: true }),
       typescript({ objectHashIgnoreUnknownHack: true })
     ],
-    external: ['react', 'react-dom']
+    external: ['react', 'react-dom', 'styled-components']
   }
-  
